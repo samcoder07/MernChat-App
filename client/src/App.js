394db6e1fc32e8ef5b02f7673baf88ff1df563b3
samarthbluelupin <145488981+samcoder07@users.noreply.github.com>
2024-02-06 09:10:59 +0530

    checkin

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Form from "./modules/Form";
+
+function App() {
+  return (
+    <div className="bg-[#edf3fc] h-screen flex justify-center items-center">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
